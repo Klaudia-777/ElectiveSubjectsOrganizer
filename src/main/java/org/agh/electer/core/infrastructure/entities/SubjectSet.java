713bda5c9851e768f254b158d1d85a012cc0bc1c
Klaudia <k.bednarz777@gmail.com>
2019-07-03 18:11:12 +0200

    id's as Strings

--- src/main/java/org/agh/electer/core/infrastructure/entities/SubjectSet.java
+++ src/main/java/org/agh/electer/core/infrastructure/entities/SubjectSet.java
@@ -13,14 +13,13 @@ import java.util.Set;
 @Entity(name="SUBJECT_SET_TABLE")
 public class SubjectSet {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="SUBJECT_SET_ID")
-    private int id;
+    private String id;
 
     @Column(name = "FIELD_OF_STUDY")
     private FieldOfStudy fieldOfStudy;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "SUBJECT_ID")
-    private Set<Subject> subjectSet;
+    private Set<Subject> electiveSubjects;
 }
